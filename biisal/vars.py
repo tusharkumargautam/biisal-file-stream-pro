dfa44125613c24a510f4f1d4c07229d4130af864
tusharkumargautam <tushar212001@gmail.com>
2024-10-05 10:29:32 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,7 +18,7 @@ class Var(object):
     name = str(getenv('name', 'gangplayer'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002211673711'))
     NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
