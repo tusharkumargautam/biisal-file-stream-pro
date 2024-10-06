17f0edb2c18285e83644863353a943825391069b
tusharkumargautam <tushar212001@gmail.com>
2024-10-06 15:10:23 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -1,4 +1,4 @@
-# (c) adarsh-goel (c) @biisal
+a# (c) adarsh-goel (c) @biisal
 import os
 from os import getenv, environ
 from dotenv import load_dotenv
@@ -19,7 +19,7 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002211673711'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002191829431'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
